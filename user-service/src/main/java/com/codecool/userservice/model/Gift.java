7e3e8647ed88b1b9257a92f94cab30a9b04e0a52
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-07-06 16:46:38 +0300

    Update GiftState attribute in Gift

--- user-service/src/main/java/com/codecool/userservice/model/Gift.java
+++ user-service/src/main/java/com/codecool/userservice/model/Gift.java
@@ -1,5 +1,10 @@
 package com.codecool.userservice.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+
+@Entity
 public class Gift {
 
     private final Long id;
@@ -8,12 +13,20 @@ public class Gift {
     private final Double value;
     private User owner;
 
+    @Enumerated
+    private GiftState state;
+
     public Gift(Long id, String name, Long photoId, Double value, User owner) {
         this.id = id;
         this.name = name;
         this.photoId = photoId;
         this.value = value;
         this.owner = owner;
+        this.state = GiftState.TO_SELL;
+    }
+
+    protected Gift() {
+        this.state = GiftState.UNDER_CONSTRUCTION;
     }
 
     public Long getId() {
@@ -34,6 +47,14 @@ public class Gift {
 
     public User getOwner() {
         return owner;
+    }
+
+    public GiftState getState() {
+        return state;
+    }
+
+    public void setState(GiftState state) {
+        this.state = state;
     }
 
     public void setOwner(User owner) {
