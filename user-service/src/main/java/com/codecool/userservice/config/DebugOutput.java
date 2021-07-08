b2ffdeedcf3b7d555aa33ec77e52e117c9eafd1e
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-07-08 20:42:03 +0300

    Basic CommandLineRunner to debug the example methods

---
+++ user-service/src/main/java/com/codecool/userservice/config/DebugOutput.java
@@ -0,0 +1,19 @@
+package com.codecool.userservice.config;
+
+import com.codecool.userservice.services.UserAddsAGiftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DebugOutput implements CommandLineRunner {
+
+    @Autowired
+    private UserAddsAGiftService testService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(testService.getAnImageForId(1L));
+        System.out.println(testService.deleteGiftById(1L));
+    }
+}
