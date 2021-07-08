4a38861efb72d526deca1bff26c4e74d081f96c2
craciun-st <67415094+craciun-st@users.noreply.github.com>
2021-07-08 20:12:56 +0300

    Add empty UserController

---
+++ user-service/src/main/java/com/codecool/userservice/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.codecool.userservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+
+}
